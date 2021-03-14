1f31e51a4e6fd2f0c3821d825e30314ccd7aa123
Alexander Izotov <a.izotov@uds.systems>
2021-03-14 04:25:08 +0200

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  if(n == null || typeof(n) != 'number') {
+      return 0;
+  }
+
+  n = n > 0 ? n : n * -1;
+
+  return +(''+n)
+      .split('')
+      .reverse()
+      .join('');
 }
